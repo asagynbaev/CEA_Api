94b33fd023ba896c8daaa2a56c921f5d2d55c805
Azim Sangybaev <sangybaev@simplifate.com>
2019-11-17 20:50:26 +0100

    New colums added

--- Migrations/20191117194951_NewColumns.Designer.cs
+++ Migrations/20191117194951_NewColumns.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WebApi.Models;
@@ -9,9 +10,10 @@ using WebApi.Models;
 namespace WebApi.Migrations
 {
     [DbContext(typeof(CeaContext))]
-    partial class CeaContextModelSnapshot : ModelSnapshot
+    [Migration("20191117194951_NewColumns")]
+    partial class NewColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20191117194951_NewColumns.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.Migrations
+{
+    public partial class NewColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Firstname",
+                table: "Employees",
+                newName: "FirstName");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Buses",
+                table: "Employees",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Hotels",
+                table: "Employees",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Shops",
+                table: "Employees",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Buses",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Hotels",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Shops",
+                table: "Employees");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Employees",
+                newName: "Firstname");
+        }
+    }
+}
