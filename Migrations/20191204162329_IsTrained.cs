aa743079f7caf6d63921408828dbc81ffd7f3941
Azim Sangybaev <sangybaev@simplifate.com>
2019-12-04 17:23:52 +0100

    migration

---
+++ Migrations/20191204162329_IsTrained.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.Migrations
+{
+    public partial class IsTrained : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Advert",
+                table: "Employees",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Cityzenship",
+                table: "Employees",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsTrained",
+                table: "Employees",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Advert",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Cityzenship",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "IsTrained",
+                table: "Employees");
+        }
+    }
+}
