44dd7c1b29e2a7a53dcccb82df6df71e52b011a8
Azim Sangybaev <sangybaev@simplifate.com>
2019-12-04 17:49:32 +0100

    new migration

---
+++ Migrations/20191204164756_IsTrainedForOthers.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.Migrations
+{
+    public partial class IsTrainedForOthers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsTrained",
+                table: "Employees");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "BusesTrain",
+                table: "Employees",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HotelsTrain",
+                table: "Employees",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ShopsTrain",
+                table: "Employees",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BusesTrain",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "HotelsTrain",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "ShopsTrain",
+                table: "Employees");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsTrained",
+                table: "Employees",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
