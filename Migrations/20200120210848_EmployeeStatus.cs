42de05012d8cf5e7e6359af9bd3396ac7ed75f91
Azim Sangybaev <sangybaev@simplifate.com>
2020-01-20 22:10:43 +0100

    migration

---
+++ Migrations/20200120210848_EmployeeStatus.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.Migrations
+{
+    public partial class EmployeeStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Status",
+                table: "Employees",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Employees",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldNullable: true);
+        }
+    }
+}
