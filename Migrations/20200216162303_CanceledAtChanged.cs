8ad33e1d74c9b046a85490113452cbdb0d5604fb
Azim Sangybaev <sangybaev@simplifate.com>
2020-02-16 17:24:31 +0100

    migration

---
+++ Migrations/20200216162303_CanceledAtChanged.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.Migrations
+{
+    public partial class CanceledAtChanged : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CanceledAt",
+                table: "Shifts",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CanceledAt",
+                table: "Shifts",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
